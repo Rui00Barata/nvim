bf743ac5a1cb9de5c1a6b43506da2fbe27fd144d
Rui00Barata <rui.barata@ubi.pt>
2024-02-18 22:13:44 +0000

    other: init and lazy profile

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,38 +1,15 @@
 return {
-  -- Theme
+	-- Theme
 	{
 		"neanias/everforest-nvim",
 		version = false,
 		lazy = false,
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
-			require('everforest').setup({background="soft"})
+			require('everforest').setup({ background = "soft" })
 			require('everforest').load()
 		end,
 	},
 
-	{import = "plugins.git"},
-
-	{import = "plugins.lsp"},
-
-	-- Fuzzy Finder (files, lsp, etc)
-	{
-		'nvim-telescope/telescope.nvim',
-		branch = '0.1.x',
-		dependencies = {
-			'nvim-lua/plenary.nvim',
-			-- Fuzzy Finder Algorithm which requires local dependencies to be built.
-			-- Only load if `make` is available. Make sure you have the system
-			-- requirements installed.
-			{
-			'nvim-telescope/telescope-fzf-native.nvim',
-			-- NOTE: If you are having trouble with this installation,
-			--       refer to the README for telescope-fzf-native for more instructions.
-			build = 'make',
-			cond = function()
-				return vim.fn.executable 'make' == 1
-			end,
-			},
-		},
-	},
+	{ import = "plugins.git" },
 }
